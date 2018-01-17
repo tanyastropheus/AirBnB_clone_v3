0e666f97c6df584e25da7a9e3ea1342afa677e66
Tanya Kryukova <ttkryukova@gmail.com>
2018-01-17 08:28:50 +0000

    updating <h4> from the dict of checked amenities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,20 +1,23 @@
 let amtys = {}; // creating an object/dict for {amty_id: amty_name}
 
-$(function () {
+function h4Update () {  // update <h4> with a list of amenities checked
+  let amtyNames = Object.values(amtys);
+  if (amtyNames.length <= 2) {
+    $('H4#amty_list').text(amtyNames.join(', '));
+  } else {
+    $('H4#amty_list').text(amtyNames.slice(0, -1).join(', ') + ', ' + amtyNames.slice(-1));
+  }
+}
+
+$(function () {  // maintains a dict of checked amenities && update them in <h4>
   $('li input:checkbox').on('click', function () {
     let id = this.dataset.id;
     let name = this.dataset.name;
-
     if (this.checked) { // if amenity is checked, add it to dict
       amtys[id] = name;
-      if (document.getElementById('amty_list').innerHTML === '&nbsp;') {
-        $('H4#amty_list').text(name);
-      } else {
-        $('H4#amty_list').append(', ' + name);
-      }
     } else { // if amentity is unchecked, remove it from dict
       delete amtys[id];
     }
-    console.log(amtys);
+    h4Update();
   });
 });
