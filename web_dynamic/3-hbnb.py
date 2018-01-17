44f615fb40c02206ae93cd5cd55ea76c40c9b5f8
jvpupcat <130@holbertonschool.com>
2018-01-17 14:44:39 +0000

    changes

---
+++ web_dynamic/3-hbnb.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Flask app to generate complete html page containing location/amenity
+dropdown menus and rental listings"""
+import uuid
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/3-hbnb')
+def display_hbnb():
+    """Generate page with popdown menu of states/cities"""
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    places = storage.all('Place')
+    cache_id = uuid.uuid4()
+    return render_template('3-hbnb.html',
+                           states=states,
+                           amenities=amenities,
+                           places=places, cache_id=cache_id)
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def post_places_search():
+    """searches for a place"""
+    if request.get_json() is not None:
+        params = request.get_json()
+        states = params.get('states', [])
+        cities = params.get('cities', [])
+        amenities = params.get('amenities', [])
+        amenity_objects = []
+        for amenity_id in amenities:
+            amenity = storage.get('Amenity', amenity_id)
+            if amenity:
+                amenity_objects.append(amenity)
+        if states == cities == []:
+            places = storage.all('Place').values()
+        else:
+            places = []
+            for state_id in states:
+                state = storage.get('State', state_id)
+                state_cities = state.cities
+                for city in state_cities:
+                    if city.id not in cities:
+                        cities.append(city.id)
+            for city_id in cities:
+                city = storage.get('City', city_id)
+                for place in city.places:
+                    places.append(place)
+        confirmed_places = []
+        for place in places:
+            place_amenities = place.amenities
+            confirmed_places.append(place.to_dict())
+            for amenity in amenity_objects:
+                if amenity not in place_amenities:
+                    confirmed_places.pop()
+                    break
+        return jsonify(confirmed_places)
+    else:
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+
+@app.teardown_appcontext
+def teardown_db(*args, **kwargs):
+    """Close database or file storage"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
