813cee81f412de7dad61c7a487ae9b7c55ca6ed5
Tanya Kryukova <ttkryukova@gmail.com>
2018-01-17 09:17:42 +0000

    set '&nbsp;' when no amenity is checked && used given selectors

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,22 +1,31 @@
 let amtys = {}; // creating an object/dict for {amty_id: amty_name}
 
-function h4Update () {  // update <h4> with a list of amenities checked
+function h4Update () { // update <h4> with a list of amenities checked
   let amtyNames = Object.values(amtys);
-  if (amtyNames.length <= 2) {
-    $('H4#amty_list').text(amtyNames.join(', '));
+  if ($.isEmptyObject(amtys)) {
+    $('div.amenities > h4').html('&nbsp;');
+  } else if (amtyNames.length <= 2) {
+    $('div.amenities > h4').text(amtyNames.join(', '));
   } else {
-    $('H4#amty_list').text(amtyNames.slice(0, -1).join(', ') + ', ' + amtyNames.slice(-1));
+    $('div.amenities > h4').text(amtyNames.slice(0, -1).join(', ') + ', ' + amtyNames.slice(-1));
   }
 }
 
-$(function () {  // maintains a dict of checked amenities && update them in <h4>
-  $('li input:checkbox').on('click', function () {
+$(function () { // maintains a dict of checked amenities && update them in <h4>
+  $('li input[type=checkbox]').on('click', function () {
     let id = this.dataset.id;
     let name = this.dataset.name;
     if (this.checked) { // if amenity is checked, add it to dict
       amtys[id] = name;
     } else { // if amentity is unchecked, remove it from dict
       delete amtys[id];
+    }
+    // update <h4> with a list of amenities checked
+    if ($.isEmptyObject(amtys)) {
+      $('div.amenities > h4').html('&nbsp;');
+    } else {
+      let amtyNames = Object.values(amtys).toString();
+      $('div.amenities > h4').html(amtyNames);
     }
     h4Update();
   });
